bb0cb3f0cd11b70501e410ff86eaa8cb776d84ee
carottee <118385001+carottee@users.noreply.github.com>
2023-12-06 07:14:14 +0100

    Update temps_sommet.c

--- temps_sommet.c
+++ temps_sommet.c
@@ -1,12 +1,1 @@
-//
-// Created by thais on 03/12/2023.
-//
 
-#include "header.h"
-
-float* temps_sommet(struct Graphe *G, float *tab, int src, float tmp) {
-
-    tab[src] = tmp ;
-
-    return tab; // Retourne le pointeur vers le premier élément du tableau
-}
